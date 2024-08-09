76247eb51e5ad27890fa8d76aa01bc2d30e1de74
quokka <qkforest@gmail.com>
2024-08-09 22:56:57 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/qkforest/dealshop/DealshopApplication.java
@@ -0,0 +1,13 @@
+package com.qkforest.dealshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealshopApplication.class, args);
+    }
+
+}
