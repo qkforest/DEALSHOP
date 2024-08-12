2afceeaab5ab7030c66028c9b54e1cba07bbe053
quokka <qkforest@gmail.com>
2024-08-12 10:22:10 +0900

    feat : 회원 가입 기능 구현
    Close #1

--- user-service/src/test/java/com/qkforest/userservice/MemberServiceApplicationTests.java
+++ user-service/src/test/java/com/qkforest/userservice/MemberServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.qkforest.dealshop;
+package com.qkforest.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DealshopApplicationTests {
+class MemberServiceApplicationTests {
 
     @Test
     void contextLoads() {
